a09c3dd4fa093110a76dd9d2c5ab0e0f3bd97fc4
rashidalam9678 <mohdrashidalam786@gmail.com>
2022-11-13 18:44:37 +0530

    added the initial files

---
+++ context/user.js
@@ -0,0 +1,25 @@
+import { createContext, useContext } from "react";
+
+// Creating the user context
+const UserContext = createContext();
+
+// Making the function which will wrap the whole app using Context Provider
+export default function AppStore({ children }) {
+  
+    const user= {
+        id: 1,
+        name: "John Doe",
+        token:"3DJ39#DFLLDF58$LKDFO#O3N4OO"
+    }
+
+  return (
+    <UserContext.Provider value={ {user} }>
+      {children}
+    </UserContext.Provider>
+  );
+}
+
+// Make useUserContext Hook to easily use our context throughout the appplication
+export function useUserContext() {
+  return useContext(UserContext);
+}
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
 import '../styles/globals.css'
+import AppStore from '../context/user'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+
+    // Wrapping our app in AppStore
+    <AppStore>
+      <Component {...pageProps} />
+    </AppStore>
+  )
 }
 
 export default MyApp
